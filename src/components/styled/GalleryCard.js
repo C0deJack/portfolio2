a94dcd1333ea0dacdd3f16ee33c67a0ed420f8a5
Jack <jackwoodpearce@gmail.com>
2021-12-11 21:26:20 +0000

    Add gsap animation package to project

--- src/components/styled/GalleryCard.js
+++ src/components/styled/GalleryCard.js
@@ -1,11 +1,14 @@
+import { useRef } from 'react';
+import gsap from 'gsap';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const StyledGalleryCard = styled.div`
-    background-color: hotpink;
+    background-color: lime;
     width: 300px;
     height: 400px;
     margin: 1rem;
+    padding: 1rem;
 
     h3 {
         color: ${props => props.theme.color.foreground};
@@ -13,8 +16,16 @@ const StyledGalleryCard = styled.div`
 `;
 
 export default function GalleryCard({ projectTitle }) {
+    const boxRef = useRef();
+
+    const handleClick = () => {
+        gsap.to(boxRef.current, {
+            rotation: '+=360',
+        });
+    };
+
     return (
-        <StyledGalleryCard>
+        <StyledGalleryCard className='card' ref={boxRef} onClick={handleClick}>
             <h3>{projectTitle}</h3>
         </StyledGalleryCard>
     );
