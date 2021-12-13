f44668d2acf8497f4f97ca22dd43d1a98866d604
Jack <jackwoodpearce@gmail.com>
2021-12-13 21:53:12 +0000

    Start animation on card flip

--- src/components/styled/GalleryCard.js
+++ src/components/styled/GalleryCard.js
@@ -1,15 +1,27 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import gsap from 'gsap';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const StyledGalleryCard = styled.div`
+    position: relative;
+    width: 300px;
+    height: 400px;
+    margin: 1rem;
+    /* transform-style: preserve-3d; */
+
     .card {
+        position: absolute;
+        /* backface-visibility: hidden; */
         background-color: lime;
         width: 300px;
         height: 400px;
         margin: 1rem;
         padding: 1rem;
+    }
+
+    .back {
+        background-color: blue;
     }
 
     h3 {
@@ -18,18 +30,27 @@ const StyledGalleryCard = styled.div`
 `;
 
 export default function GalleryCard({ projectTitle }) {
-    const boxRef = useRef();
+    const frontRef = useRef();
+    const backRef = useRef();
 
     const handleClick = () => {
-        gsap.to(boxRef.current, {
-            rotation: '+=360',
+        gsap.to(frontRef.current, {
+            rotationY: 180,
+            duration: 1,
+            ease: 'back.out(1.7)',
         });
     };
+
+    useEffect(() => {
+        gsap.set(backRef.current, {
+            rotationY: 180,
+        });
+    }, []);
 
     const handleOnHover = isMouseHovering => {
         const rotation = isMouseHovering ? 180 : 0;
 
-        gsap.to(boxRef.current, {
+        gsap.to([frontRef.current, backRef.current], {
             rotationY: rotation,
             duration: 1,
             ease: 'back.out(1.7)',
@@ -46,8 +67,11 @@ export default function GalleryCard({ projectTitle }) {
                 handleOnHover(false);
             }}
         >
-            <div className='card' ref={boxRef}>
-                <h3>{projectTitle}</h3>
+            <div className='card front' ref={frontRef}>
+                <h3>{projectTitle} FRONT</h3>
+            </div>
+            <div className='card back' ref={backRef}>
+                <h3>{projectTitle} BACK</h3>
             </div>
         </StyledGalleryCard>
     );
